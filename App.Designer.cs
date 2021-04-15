c09a1479bd4ff7c56f2f61981f91bbfa6858edeb
Switchback <aidartemirgaliev@gmail.com>
2021-04-16 04:25:59 +0600

    MLE bit choice

--- App.Designer.cs
+++ App.Designer.cs
@@ -24,65 +24,67 @@ namespace MultiLayered_Encryptor_App {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label_InputText = new System.Windows.Forms.Label();
+            this.label_OutputText = new System.Windows.Forms.Label();
+            this.textBox_InputText = new System.Windows.Forms.TextBox();
+            this.textBox_OutputText = new System.Windows.Forms.TextBox();
+            this.button_Transform = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox_Algorithm = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox_TypeToPerform = new System.Windows.Forms.ListBox();
+            this.label_MLE = new System.Windows.Forms.Label();
+            this.listBox_MLEbits = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // label1
+            // label_InputText
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.label1.Location = new System.Drawing.Point(12, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Enter your text";
+            this.label_InputText.AutoSize = true;
+            this.label_InputText.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label_InputText.Location = new System.Drawing.Point(12, 60);
+            this.label_InputText.Name = "label_InputText";
+            this.label_InputText.Size = new System.Drawing.Size(152, 24);
+            this.label_InputText.TabIndex = 0;
+            this.label_InputText.Text = "Enter your text";
             // 
-            // label2
+            // label_OutputText
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.label2.Location = new System.Drawing.Point(13, 263);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Result: ";
+            this.label_OutputText.AutoSize = true;
+            this.label_OutputText.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label_OutputText.Location = new System.Drawing.Point(13, 263);
+            this.label_OutputText.Name = "label_OutputText";
+            this.label_OutputText.Size = new System.Drawing.Size(83, 24);
+            this.label_OutputText.TabIndex = 1;
+            this.label_OutputText.Text = "Result: ";
             // 
-            // textBox1
+            // textBox_InputText
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 87);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(540, 163);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "Text Box";
+            this.textBox_InputText.Location = new System.Drawing.Point(13, 87);
+            this.textBox_InputText.Multiline = true;
+            this.textBox_InputText.Name = "textBox_InputText";
+            this.textBox_InputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_InputText.Size = new System.Drawing.Size(540, 163);
+            this.textBox_InputText.TabIndex = 4;
+            this.textBox_InputText.Text = "Text Box";
             // 
-            // textBox2
+            // textBox_OutputText
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 290);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(540, 163);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "Text Box 2";
+            this.textBox_OutputText.Location = new System.Drawing.Point(13, 290);
+            this.textBox_OutputText.Multiline = true;
+            this.textBox_OutputText.Name = "textBox_OutputText";
+            this.textBox_OutputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_OutputText.Size = new System.Drawing.Size(540, 163);
+            this.textBox_OutputText.TabIndex = 5;
+            this.textBox_OutputText.Text = "Text Box 2";
             // 
-            // button1
+            // button_Transform
             // 
-            this.button1.Location = new System.Drawing.Point(407, 459);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 41);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Transform";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_Transform.Location = new System.Drawing.Point(407, 459);
+            this.button_Transform.Name = "button_Transform";
+            this.button_Transform.Size = new System.Drawing.Size(146, 41);
+            this.button_Transform.TabIndex = 6;
+            this.button_Transform.Text = "Transform";
+            this.button_Transform.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -94,14 +96,14 @@ namespace MultiLayered_Encryptor_App {
             this.label3.TabIndex = 7;
             this.label3.Text = "Algorithm:";
             // 
-            // listBox1
+            // listBox_Algorithm
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(329, 18);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(224, 24);
-            this.listBox1.TabIndex = 8;
+            this.listBox_Algorithm.FormattingEnabled = true;
+            this.listBox_Algorithm.ItemHeight = 20;
+            this.listBox_Algorithm.Location = new System.Drawing.Point(329, 18);
+            this.listBox_Algorithm.Name = "listBox_Algorithm";
+            this.listBox_Algorithm.Size = new System.Drawing.Size(224, 24);
+            this.listBox_Algorithm.TabIndex = 8;
             // 
             // label4
             // 
@@ -113,30 +115,51 @@ namespace MultiLayered_Encryptor_App {
             this.label4.TabIndex = 9;
             this.label4.Text = "Do";
             // 
-            // listBox2
+            // listBox_TypeToPerform
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(61, 18);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(112, 24);
-            this.listBox2.TabIndex = 10;
+            this.listBox_TypeToPerform.FormattingEnabled = true;
+            this.listBox_TypeToPerform.ItemHeight = 20;
+            this.listBox_TypeToPerform.Location = new System.Drawing.Point(61, 18);
+            this.listBox_TypeToPerform.Name = "listBox_TypeToPerform";
+            this.listBox_TypeToPerform.Size = new System.Drawing.Size(112, 24);
+            this.listBox_TypeToPerform.TabIndex = 10;
             // 
-            // Form1
+            // label_MLE
+            // 
+            this.label_MLE.AutoSize = true;
+            this.label_MLE.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label_MLE.Location = new System.Drawing.Point(215, 60);
+            this.label_MLE.Name = "label_MLE";
+            this.label_MLE.Size = new System.Drawing.Size(255, 24);
+            this.label_MLE.TabIndex = 11;
+            this.label_MLE.Text = "Multi-Layered Encryption:";
+            // 
+            // listBox_MLEbits
+            // 
+            this.listBox_MLEbits.FormattingEnabled = true;
+            this.listBox_MLEbits.ItemHeight = 20;
+            this.listBox_MLEbits.Location = new System.Drawing.Point(476, 60);
+            this.listBox_MLEbits.Name = "listBox_MLEbits";
+            this.listBox_MLEbits.Size = new System.Drawing.Size(77, 24);
+            this.listBox_MLEbits.TabIndex = 12;
+            // 
+            // App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(568, 512);
-            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox_MLEbits);
+            this.Controls.Add(this.label_MLE);
+            this.Controls.Add(this.listBox_TypeToPerform);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox_Algorithm);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Controls.Add(this.button_Transform);
+            this.Controls.Add(this.textBox_OutputText);
+            this.Controls.Add(this.textBox_InputText);
+            this.Controls.Add(this.label_OutputText);
+            this.Controls.Add(this.label_InputText);
+            this.Name = "App";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -146,15 +169,17 @@ namespace MultiLayered_Encryptor_App {
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_InputText;
+        private System.Windows.Forms.Label label_OutputText;
+        private System.Windows.Forms.TextBox textBox_InputText;
+        private System.Windows.Forms.TextBox textBox_OutputText;
+        private System.Windows.Forms.Button button_Transform;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox_Algorithm;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox_TypeToPerform;
+        private System.Windows.Forms.Label label_MLE;
+        private System.Windows.Forms.ListBox listBox_MLEbits;
     }
 }
 
