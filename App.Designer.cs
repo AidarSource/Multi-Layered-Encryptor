fac9642c15c59befff281aa7b96e224cbb327a67
Switchback <aidartemirgaliev@gmail.com>
2021-04-19 02:01:55 +0600

    + 1 algorithm to encrypt/decrypt data

--- App.Designer.cs
+++ App.Designer.cs
@@ -30,16 +30,18 @@ namespace MultiLayered_Encryptor_App {
             this.textBox_OutputText = new System.Windows.Forms.TextBox();
             this.button_Transform = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.listBox_Algorithm = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.listBox_TypeToPerform = new System.Windows.Forms.ListBox();
             this.label_MLE = new System.Windows.Forms.Label();
-            this.listBox_MLEbits = new System.Windows.Forms.ListBox();
+            this.comboBox_TypeToPerform = new System.Windows.Forms.ComboBox();
+            this.comboBox_Algorithm = new System.Windows.Forms.ComboBox();
+            this.comboBox_MLEBits = new System.Windows.Forms.ComboBox();
+            this.label_ProcessTracker = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_InputText
             // 
             this.label_InputText.AutoSize = true;
+            this.label_InputText.BackColor = System.Drawing.Color.Transparent;
             this.label_InputText.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.label_InputText.Location = new System.Drawing.Point(12, 60);
             this.label_InputText.Name = "label_InputText";
@@ -50,8 +52,9 @@ namespace MultiLayered_Encryptor_App {
             // label_OutputText
             // 
             this.label_OutputText.AutoSize = true;
+            this.label_OutputText.BackColor = System.Drawing.Color.Transparent;
             this.label_OutputText.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.label_OutputText.Location = new System.Drawing.Point(13, 263);
+            this.label_OutputText.Location = new System.Drawing.Point(14, 263);
             this.label_OutputText.Name = "label_OutputText";
             this.label_OutputText.Size = new System.Drawing.Size(83, 24);
             this.label_OutputText.TabIndex = 1;
@@ -85,10 +88,12 @@ namespace MultiLayered_Encryptor_App {
             this.button_Transform.TabIndex = 6;
             this.button_Transform.Text = "Transform";
             this.button_Transform.UseVisualStyleBackColor = true;
+            this.button_Transform.Click += new System.EventHandler(this.button_Transform_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.label3.Location = new System.Drawing.Point(215, 18);
             this.label3.Name = "label3";
@@ -96,18 +101,10 @@ namespace MultiLayered_Encryptor_App {
             this.label3.TabIndex = 7;
             this.label3.Text = "Algorithm:";
             // 
-            // listBox_Algorithm
-            // 
-            this.listBox_Algorithm.FormattingEnabled = true;
-            this.listBox_Algorithm.ItemHeight = 20;
-            this.listBox_Algorithm.Location = new System.Drawing.Point(329, 18);
-            this.listBox_Algorithm.Name = "listBox_Algorithm";
-            this.listBox_Algorithm.Size = new System.Drawing.Size(224, 24);
-            this.listBox_Algorithm.TabIndex = 8;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.label4.Location = new System.Drawing.Point(13, 18);
             this.label4.Name = "label4";
@@ -115,18 +112,10 @@ namespace MultiLayered_Encryptor_App {
             this.label4.TabIndex = 9;
             this.label4.Text = "Do";
             // 
-            // listBox_TypeToPerform
-            // 
-            this.listBox_TypeToPerform.FormattingEnabled = true;
-            this.listBox_TypeToPerform.ItemHeight = 20;
-            this.listBox_TypeToPerform.Location = new System.Drawing.Point(61, 18);
-            this.listBox_TypeToPerform.Name = "listBox_TypeToPerform";
-            this.listBox_TypeToPerform.Size = new System.Drawing.Size(112, 24);
-            this.listBox_TypeToPerform.TabIndex = 10;
-            // 
             // label_MLE
             // 
             this.label_MLE.AutoSize = true;
+            this.label_MLE.BackColor = System.Drawing.Color.Transparent;
             this.label_MLE.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.label_MLE.Location = new System.Drawing.Point(215, 60);
             this.label_MLE.Name = "label_MLE";
@@ -134,25 +123,72 @@ namespace MultiLayered_Encryptor_App {
             this.label_MLE.TabIndex = 11;
             this.label_MLE.Text = "Multi-Layered Encryption:";
             // 
-            // listBox_MLEbits
+            // comboBox_TypeToPerform
             // 
-            this.listBox_MLEbits.FormattingEnabled = true;
-            this.listBox_MLEbits.ItemHeight = 20;
-            this.listBox_MLEbits.Location = new System.Drawing.Point(476, 60);
-            this.listBox_MLEbits.Name = "listBox_MLEbits";
-            this.listBox_MLEbits.Size = new System.Drawing.Size(77, 24);
-            this.listBox_MLEbits.TabIndex = 12;
+            this.comboBox_TypeToPerform.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_TypeToPerform.FormattingEnabled = true;
+            this.comboBox_TypeToPerform.Items.AddRange(new object[] {
+            "Encrypt",
+            "Decrypt"});
+            this.comboBox_TypeToPerform.Location = new System.Drawing.Point(55, 14);
+            this.comboBox_TypeToPerform.Name = "comboBox_TypeToPerform";
+            this.comboBox_TypeToPerform.Size = new System.Drawing.Size(109, 28);
+            this.comboBox_TypeToPerform.TabIndex = 13;
+            // 
+            // comboBox_Algorithm
+            // 
+            this.comboBox_Algorithm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Algorithm.FormattingEnabled = true;
+            this.comboBox_Algorithm.Items.AddRange(new object[] {
+            "Multi-Layered Encryption",
+            "RSA-Asymmetric Encryption"});
+            this.comboBox_Algorithm.Location = new System.Drawing.Point(329, 14);
+            this.comboBox_Algorithm.Name = "comboBox_Algorithm";
+            this.comboBox_Algorithm.Size = new System.Drawing.Size(224, 28);
+            this.comboBox_Algorithm.Sorted = true;
+            this.comboBox_Algorithm.TabIndex = 14;
+            // 
+            // comboBox_MLEBits
+            // 
+            this.comboBox_MLEBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_MLEBits.FormattingEnabled = true;
+            this.comboBox_MLEBits.Items.AddRange(new object[] {
+            "2 bit",
+            "4 bit",
+            "8 bit",
+            "16 bit",
+            "32 bit"});
+            this.comboBox_MLEBits.Location = new System.Drawing.Point(468, 56);
+            this.comboBox_MLEBits.Name = "comboBox_MLEBits";
+            this.comboBox_MLEBits.Size = new System.Drawing.Size(85, 28);
+            this.comboBox_MLEBits.TabIndex = 15;
+            // 
+            // label_ProcessTracker
+            // 
+            this.label_ProcessTracker.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label_ProcessTracker.BackColor = System.Drawing.Color.Transparent;
+            this.label_ProcessTracker.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_ProcessTracker.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label_ProcessTracker.Location = new System.Drawing.Point(14, 466);
+            this.label_ProcessTracker.Name = "label_ProcessTracker";
+            this.label_ProcessTracker.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_ProcessTracker.Size = new System.Drawing.Size(387, 24);
+            this.label_ProcessTracker.TabIndex = 16;
+            this.label_ProcessTracker.Text = "Processed in ";
+            this.label_ProcessTracker.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label_ProcessTracker.Visible = false;
             // 
             // App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(568, 512);
-            this.Controls.Add(this.listBox_MLEbits);
+            this.Controls.Add(this.label_ProcessTracker);
+            this.Controls.Add(this.comboBox_MLEBits);
+            this.Controls.Add(this.comboBox_Algorithm);
+            this.Controls.Add(this.comboBox_TypeToPerform);
             this.Controls.Add(this.label_MLE);
-            this.Controls.Add(this.listBox_TypeToPerform);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.listBox_Algorithm);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button_Transform);
             this.Controls.Add(this.textBox_OutputText);
@@ -175,11 +211,12 @@ namespace MultiLayered_Encryptor_App {
         private System.Windows.Forms.TextBox textBox_OutputText;
         private System.Windows.Forms.Button button_Transform;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox_Algorithm;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ListBox listBox_TypeToPerform;
         private System.Windows.Forms.Label label_MLE;
-        private System.Windows.Forms.ListBox listBox_MLEbits;
+        private System.Windows.Forms.ComboBox comboBox_TypeToPerform;
+        private System.Windows.Forms.ComboBox comboBox_Algorithm;
+        private System.Windows.Forms.ComboBox comboBox_MLEBits;
+        private System.Windows.Forms.Label label_ProcessTracker;
     }
 }
 
--- App.cs
+++ App.cs
@@ -7,23 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace MultiLayered_Encryptor_App {
     public partial class App : Form {
         public App() {
             InitializeComponent();
-            this.BackgroundImage = Properties.Resources.linux;
+            this.BackgroundImage = Properties.Resources.mle;
             BackgroundImageLayout = ImageLayout.Stretch;
+
+
         }
 
 
         private void Form1_Load(object sender, EventArgs e) {
-            bool bIsMLE = true;
-            if(!bIsMLE) {
-                label_MLE.Dispose();
-                listBox_MLEbits.Dispose();
+            bool bIsMLE = false;
+            // checking for Multi-Layered Encryption in Algorithms
+            if(comboBox_Algorithm.Text != "Multi-Layered Encryption") {
+                label_MLE.Visible = false;
+                comboBox_MLEBits.Visible = false;
+            } else if(comboBox_Algorithm.Text != "Multi-Layered Encryption") {
+                label_MLE.Visible = true;
+                comboBox_MLEBits.Visible = true;
             }
             
+        }
+
+        private void button_Transform_Click(object sender, EventArgs e) {
+            // RSA-Asymmetric Encryption
+            if (comboBox_Algorithm.Text == "RSA-Asymmetric Encryption") {
+                if (comboBox_TypeToPerform.Text == "Encrypt") {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    label_ProcessTracker.Visible = true;
+                    label_ProcessTracker.Text = "Processing . . .";
+                    CipherAlgorithms.plaintext = CipherAlgorithms.ByteConverter.GetBytes(textBox_InputText.Text);
+                    CipherAlgorithms.encryptedtext = CipherAlgorithms.Encryption(CipherAlgorithms.plaintext, CipherAlgorithms.RSA.ExportParameters(false), false);
+                    textBox_OutputText.Text = CipherAlgorithms.ByteConverter.GetString(CipherAlgorithms.encryptedtext);
+                    stopwatch.Stop();
+                    label_ProcessTracker.Text = String.Format("Completed in {0} seconds.", stopwatch.ElapsedMilliseconds / 1000.0);
+                    label_ProcessTracker.Text = CipherAlgorithms.key;
+                } else if(comboBox_TypeToPerform.Text == "Decrypt") {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    label_ProcessTracker.Visible = true;
+                    label_ProcessTracker.Text = "Processing . . .";
+                    byte[] decryptedtext = CipherAlgorithms.Decryption(CipherAlgorithms.encryptedtext, CipherAlgorithms.RSA.ExportParameters(true), false);
+                    textBox_OutputText.Text = CipherAlgorithms.ByteConverter.GetString(decryptedtext);
+                    stopwatch.Stop();
+                    label_ProcessTracker.Text = String.Format("Completed in {0} seconds.", stopwatch.ElapsedMilliseconds / 1000.0);
+                }
+            }
         }
     }
 }
