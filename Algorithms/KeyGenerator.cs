918e7031fbc912972e9764b0018ef3c95df321e2
Switchback <aidartemirgaliev@gmail.com>
2021-04-26 19:00:04 +0600

    random key generator

---
+++ Algorithms/KeyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace MultiLayered_Encryptor_App.Algorithms {
+    class KeyGenerator {
+        public static string GetRandomAlphanumericString(int length) {
+            const string alphanumericCharacters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "abcdefghijklmnopqrstuvwxyz" +
+                "0123456789";
+                //+ "!@#$^&*()_";
+            return GetRandomString(length, alphanumericCharacters);
+        }
+        public static string GetRandomString(int length, IEnumerable<char> characterSet) {
+            if (length < 0)
+                throw new ArgumentException("length must not be negative", "length");
+            if (length > int.MaxValue / 8) // 250 million chars ought to be enough for anybody
+                throw new ArgumentException("length is too big", "length");
+            if (characterSet == null)
+                throw new ArgumentNullException("characterSet");
+            var characterArray = characterSet.Distinct().ToArray();
+            if (characterArray.Length == 0)
+                throw new ArgumentException("characterSet must not be empty", "characterSet");
+
+            var bytes = new byte[length * 8];
+            new RNGCryptoServiceProvider().GetBytes(bytes);
+            var result = new char[length];
+            for (int i = 0; i < length; i++) {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = characterArray[value % (uint)characterArray.Length];
+            }
+            return new string(result);
+        }
+    }
+}
